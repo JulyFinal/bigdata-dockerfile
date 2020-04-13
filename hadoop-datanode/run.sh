c8033a95e7cde21c115fd77db421fee78036f50b
julyfinal <julyfinal@outlook.com>
2020-04-13 16:39:33 +0800

    fix file

--- hadoop-datanode/run.sh
+++ hadoop-datanode/run.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-namedir=`echo $HDFS_CONF_dfs_namenode_name_dir | perl -pe 's#file://##'`
-if [ ! -d $namedir ]; then
-  echo "Namenode name directory not found: $namedir"
+datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
+if [ ! -d $datadir ]; then
+  echo "Datanode data directory not found: $datadir"
   exit 2
 fi
 
-if [ -z "$CLUSTER_NAME" ]; then
-  echo "Cluster name not specified"
-  exit 2
-fi
-
-if [ "`ls -A $namedir`" == "" ]; then
-  echo "Formatting namenode name directory: $namedir"
-  $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format $CLUSTER_NAME
-fi
-
-$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
